4d291b9653ec66e1a3b0a3c72c6d41239b9389c0
wicahma <teguh.dc.kusuma@gmail.com>
2024-04-07 15:56:45 +0700

    update: set getter for data token

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,54 +1,55 @@
-import axios from 'axios';
+import axios from 'axios'
 
-const env = import.meta.env;
+const env = import.meta.env
 
 const authModules = {
   state: {
-    user: JSON.parse(localStorage.getItem('user')) || null,
-    
+    user: JSON.parse(localStorage.getItem('user')) || null
   },
 
   getters: {
-    admin: state => state.user
+    admin: (state) => state.user.data,
+    getAccessToken: (state) => {
+      return state.user.tokens.access
+    },
+    getRefreshToken: (state) => state.user.tokens.refresh
   },
 
   mutations: {
     setUser(state, user) {
-      state.user = user;
-      localStorage.setItem('user', JSON.stringify(user));
+      state.user = user
+      localStorage.setItem('user', JSON.stringify(user))
     },
     setAccessToken(state, token) {
-      localStorage.setItem('accessToken', JSON.stringify(token));
+      localStorage.setItem('accessToken', JSON.stringify(token))
     },
     setRefreshToken(state, token) {
-      localStorage.setItem('refreshToken', JSON.stringify(token));
-    },
-    
+      localStorage.setItem('refreshToken', JSON.stringify(token))
+    }
   },
   actions: {
     async loginUser({ commit }, form) {
       try {
-        const response = await axios.post(`${env.VITE_API_BASE_URL}/auth/login`, form );
-        commit('setUser', response.data);
+        const response = await axios.post(`${env.VITE_API_BASE_URL}/auth/login`, form)
+        commit('setUser', response.data)
         // Simpan user dan token ke dalam state Vuex dan local storage
-        commit('setAccessToken', response.data.tokens.access);
-        commit('setRefreshToken', response.data.tokens.refresh); // assuming the response has a 'user' field
-        return response.data; // returning data if needed
+        commit('setAccessToken', response.data.tokens.access)
+        commit('setRefreshToken', response.data.tokens.refresh) // assuming the response has a 'user' field
+        return response.data // returning data if needed
       } catch (error) {
-        return error.response;
+        return error.response
       }
     },
 
-    async registerUser(state, form){
+    async registerUser(state, form) {
       try {
-        const response = await axios.post(`${env.VITE_API_BASE_URL}/auth/register`, form);
-        return response.data;
+        const response = await axios.post(`${env.VITE_API_BASE_URL}/auth/register`, form)
+        return response.data
       } catch (error) {
-        return error.message;
+        return error.message
       }
     }
-  },
-  
-};
+  }
+}
 
 export default authModules
