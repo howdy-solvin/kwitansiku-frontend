a96c048d069492fe00a994a4ed4fc295f4f56cb9
wicahma <teguh.dc.kusuma@gmail.com>
2024-04-07 20:17:33 +0700

    update: change print behaviour

--- src/components/ModalBase.vue
+++ src/components/ModalBase.vue
@@ -1,18 +1,20 @@
 <script>
-import CetakKuitansi from "./icons/CetakKuitansi.vue";
-import CloseIcon from "./icons/CloseIcon.vue";
+import CetakKuitansi from './icons/CetakKuitansi.vue'
+import CloseIcon from './icons/CloseIcon.vue'
+import { ref } from 'vue'
 
 export default {
-  name: "ModalBase",
+  name: 'ModalBase',
   components: { CloseIcon, CetakKuitansi },
   props: { tooglePopUp: Boolean, showPrintButton: Boolean },
   setup() {
+    const printSection = ref()
     const printModal = () => {
-      window.print();
-    };
-    return { printModal };
-  },
-};
+      window.print()
+    }
+    return { printModal, printSection }
+  }
+}
 </script>
 
 <template>
@@ -21,37 +23,71 @@ export default {
   >
     <div class="fixed top-0 bottom-0 left-0 right-0" @click="$emit('close-modal')" />
     <main
-      class="w-[750px] h-[90%] relative flex flex-col rounded-xl overflow-hidden bg-white self-center transition duration-150 ease-in-out"
+      class="w-[750px] print-view h-[90%] relative flex flex-col rounded-xl overflow-hidden bg-white self-center transition duration-150 ease-in-out"
     >
       <section class="flex justify-between p-6 w-full border-b-2">
         <slot name="header" />
         <div class="flex gap-3">
           <button
-          v-if="showPrintButton"
-          @click="printModal"
-          class="bg-[#0075FF] p-[17px] rounded-lg"
+            v-if="showPrintButton"
+            @click="printModal"
+            class="bg-[#0075FF] p-[17px] rounded-lg"
           >
-          <CetakKuitansi></CetakKuitansi>
-        </button>
-        <button @click="tooglePopUp()" class="bg-[#F00000] p-[17px] rounded-lg">
-          <CloseIcon></CloseIcon>
-        </button>
+            <CetakKuitansi></CetakKuitansi>
+          </button>
+          <button @click="tooglePopUp()" class="bg-[#F00000] p-[17px] rounded-lg">
+            <CloseIcon></CloseIcon>
+          </button>
         </div>
       </section>
-
-      <section class="bg-slate-300 p-6 overflow-y-auto">
-        <div>
-          <slot name="banner"></slot>
+      <section class="overflow-y-auto">
+        <div class="bg-slate-300 p-6 relative" ref="printSection" id="kwitansi-section">
+          <slot name="banner" />
+          <slot name="main" />
         </div>
-        <slot name="main"></slot>
+        <div class="mt-10 p-6 bg-slate-300" id="pasien-section">
+          <slot name="banner" />
+          <slot name="pasien" />
+        </div>
       </section>
     </main>
   </div>
 </template>
-<style scope="this api replaced by slot-scope in 2.5.0+">
+<style>
 @media print {
+  html,
+  body {
+    height: 100%;
+    overflow: hidden;
+    visibility: hidden;
+    font-size: 12px;
+  }
+
   @page {
-    font-size: 20pt;
+    size: 21.7cm 13.8cm;
+    margin: 0 30px !important;
+  }
+
+  #pasien {
+    /* page-break-after: always;
+    break-after: page; */
+  }
+
+  .print-view {
+    width: 100vw;
+    /* height: 100%; */
+    margin: 0 3%;
+  }
+
+  #kwitansi-section {
+    display: block;
+    width: 100%;
+    /* height: 100%; */
+    visibility: visible !important;
+    page-break-after: always;
+    position: absolute;
+    left: 0;
+    top: 0;
   }
 }
 </style>
