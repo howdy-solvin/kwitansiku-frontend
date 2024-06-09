cc065e2f397e7ad895c7a06d066e9b3fe792f613
akbar5yn <akbar2000016129@webmail.uad.ac.id>
2024-06-09 17:56:29 +0700

    added fontawesome package

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 
 const app = createApp(App)
