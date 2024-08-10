31c597150069e8ab7b7e6307f7c230623163602d
wicahma <teguh.dc.kusuma@gmail.com>
2024-08-10 10:35:05 +0700

    update: assign new route for print page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 // router.js
-import { createRouter, createWebHistory } from 'vue-router';
-import TheWelcome from '@/views/TheWelcome.vue';
-import LoginView from '@/views/LoginView.vue';
-import DashboardView from '@/views/DashboardView.vue';
-import HomeView from '@/views/HomeView.vue';
-import PasienTki from '@/views/pasien/PasienTki.vue';
-import PasienUmum from '@/views/pasien/PasienUmum.vue';
-import RekapKwitansi from '@/views/RekapKwitansi.vue';
-import BlankoKwitansi from '@/views/BlankoView.vue';
-import store from '@/store';
+import { createRouter, createWebHistory } from 'vue-router'
+import TheWelcome from '@/views/TheWelcome.vue'
+import LoginView from '@/views/LoginView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import HomeView from '@/views/HomeView.vue'
+import PasienTki from '@/views/pasien/PasienTki.vue'
+import PasienUmum from '@/views/pasien/PasienUmum.vue'
+import RekapKwitansi from '@/views/RekapKwitansi.vue'
+import BlankoKwitansi from '@/views/BlankoView.vue'
+import store from '@/store'
+import BlankoPraView from '@/views/print/BlankoPraView.vue'
+import BlankoFullView from '@/views/print/BlankoFullView.vue'
+import KwitansiView from '@/views/print/KwitansiView.vue'
 
 const routes = [
   { path: '/', name: 'Welcome', component: TheWelcome },
@@ -19,67 +22,80 @@ const routes = [
     component: DashboardView,
     children: [
       {
-        path: "",
+        path: '',
         name: 'Home',
-        component: HomeView,
+        component: HomeView
       },
       {
         path: '/pasien-tki',
         name: 'Tki',
-        component: PasienTki,
+        component: PasienTki
       },
       {
         path: '/pasien-umum',
         name: 'Umum',
-        component: PasienUmum,
+        component: PasienUmum
       },
       {
         path: '/rekap-kwitansi',
         name: 'RekapKwitansi',
-        component: RekapKwitansi,
+        component: RekapKwitansi
       },
       {
         path: '/blanko-kwitansi',
         name: 'BlankoKwitansi',
-        component: BlankoKwitansi,
+        component: BlankoKwitansi
       }
     ]
   },
-];
+  {
+    path: '/print/blanko-pra',
+    name: 'PrintBlankoPra',
+    component: BlankoPraView,
+  },
+  {
+    path: '/print/blanko-full',
+    name: 'PrintBlankoFull',
+    component: BlankoFullView,
+  },
+  {
+    path: '/print/kwitansi',
+    name: 'PrintKwitansi',
+    component: KwitansiView,
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
-});
-
+  routes
+})
 
 router.beforeEach(async (to, from, next) => {
-  let isAuthenticated = store.getters['isAuthenticated'];
-  let token = localStorage.getItem('accessToken');
+  let isAuthenticated = store.getters['isAuthenticated']
+  let token = localStorage.getItem('accessToken')
 
   if (token && (await store.dispatch('isTokenExpired', token))) {
     try {
-      await store.dispatch('refreshToken');
-      isAuthenticated = true;
+      await store.dispatch('refreshToken')
+      isAuthenticated = true
     } catch (error) {
-      store.commit('clearAuthData');
-      isAuthenticated = false;
+      store.commit('clearAuthData')
+      isAuthenticated = false
     }
   }
 
   if (!isAuthenticated) {
     if (to.name !== 'Login' && to.name !== 'Register' && to.name !== 'Welcome') {
-      next({ name: 'Login' });
+      next({ name: 'Login' })
     } else {
-      next();
+      next()
     }
   } else if (to.name === 'Login' || to.name === 'Register' || to.name === 'Welcome') {
-    next({ name: 'Home' });
-    console.log('login successful');
+    next({ name: 'Home' })
+    console.log('login successful')
   } else {
-    next();
+    next()
   }
-});
+})
 
-
-export default router;
+export default router
